4b8f581d110c6ce984cc13437206c531600ad10b
“thisnabeel” <rockystorm@gmail.com>
2023-07-04 01:44:51 -0700

    Added Expected Type

--- app/serializers/algorithm_serializer.rb
+++ app/serializers/algorithm_serializer.rb
@@ -1,3 +1,19 @@
 class AlgorithmSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :expected, :difficulty
+  attributes :expected_with_type
+
+  def expected_with_type
+    x = object.expected
+    if x.starts_with? "STRING"
+      return x.split("STRING ")[1]
+    end
+
+    if x.starts_with? "INT"
+      return x.split("INT ")[1]
+    end
+
+    if x.starts_with? "BOOLEAN"
+      return x.split("BOOLEAN ")[1]
+    end
+  end
 end
