15f13c76e7e098aa800f531e29f2cfce4ad6185e
thisnabeel <nabeel.khan.swe@gmail.com>
2025-05-25 21:53:05 -0700

    mvp ready

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -7,7 +7,10 @@ class Quest < ApplicationRecord
   validates :difficulty, presence: true, numericality: { only_integer: true }
 
   def self.popular
-    quests = Quest.includes(quest_steps: :quest_step_choices).to_a
+    quests = Quest.includes(quest_steps: :quest_step_choices)
+                 .where.not(quest_steps: { image_url: [nil, ''] })
+                 .distinct
+                 .to_a
     quests.sort_by { |quest| quest.quest_steps.size }.reverse.first(10)
   end
 end 
