2d15e1c0b0912f4dd5c0a9eb74cabf3c1bafda8e
thisnabeel <nabeel.khan.swe@gmail.com>
2025-06-19 15:06:22 -0700

    added popular

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -8,9 +8,10 @@ class Quest < ApplicationRecord
 
   def self.popular(limit = 10)
     quests = Quest.includes(quest_steps: :quest_step_choices)
+                 .where.not(questable_id: [nil])
                  .where.not(quest_steps: { image_url: [nil, ''] })
                  .distinct
                  .to_a
     quests.sort_by { |quest| quest.quest_steps.size }.reverse.first(limit)
   end
-end 
+end
